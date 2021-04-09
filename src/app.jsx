11468e851a94358b0cb341682a4f46e830a4a7d9
hangueira <hks713@naver.com>
2021-04-09 15:41:07 +0900

    make youtube clone use react

---
+++ src/app.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import "./app.css";
+import VideoList from "./components/video_list/video_list";
+
+function App() {
+  const [videos, setVideos] = useState([]);
+  // mount가 되었을때만
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyB0V6GFzPUZR_Ox7kd3pAMRHMGe8JFFzn8",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <VideoList videos={videos} />;
+}
+
+export default App;
