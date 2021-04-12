a3cf3775bf1a82f104678d7d2b4d0663eabd82b7
hangueira <hks713@naver.com>
2021-04-12 19:19:10 +0900

    video add

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,47 @@
 import { useEffect, useState } from "react";
 import style from "./app.module.css";
 import SearchHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 import VideoList from "./components/video_list/video_list";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
+
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
+  };
+
   // mount가 되었을때만
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyB0V6GFzPUZR_Ox7kd3pAMRHMGe8JFFzn8",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
-  }, []);
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
+  }, [youtube]);
 
   return (
     <div className={style.app}>
-      <SearchHeader />
-      <VideoList videos={videos} />
+      <SearchHeader onSearch={search} />
+      <section className={style.content}>
+        {selectedVideo && (
+          <div className={style.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={style.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
     </div>
   );
 }
